6e6aec75866177bf0c1162d6bdabdcf0a17ff390
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-02 18:49:35 +0545

    Cropping Images in Matplotlib

--- Matplotlib/6.WorkingWithImages.py
+++ Matplotlib/6.WorkingWithImages.py
@@ -6,4 +6,9 @@ import matplotlib.pyplot as plt
 img = mpimg.imread('Image.jpg')
 plt.imshow(img)
 plt.axis('off')
+
+# Cropped Image
+cropped_img = img[100:400, 100:400]
+plt.imshow(cropped_img)
+plt.axis('off')
 plt.show()
