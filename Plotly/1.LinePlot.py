acfa05511daaefe5fef19bcf27a30f39d9e1acb8
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-04 07:18:10 +0545

    Line Plot in Plotly

---
+++ Plotly/1.LinePlot.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cufflinks as cf
+import plotly.express as px
+import plotly.io as pio
+
+tips = sns.load_dataset('tips')
+
+pio.renderers.default = 'browser'
+
+# Using Plotly Express to create a line plot
+fig = px.line(tips, x='total_bill', y='tip', title='Total Bill vs Tip')
+fig.show()
