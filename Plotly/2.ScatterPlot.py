036a700f98f47e68384549e94f9087956cc11bd9
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-04 07:21:33 +0545

    Scatter Plot in Plotly

---
+++ Plotly/2.ScatterPlot.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cufflinks as cf
+import plotly.express as px
+import plotly.io as pio
+
+tips = sns.load_dataset('tips')
+
+pio.renderers.default = 'browser'
+
+# Using Plotly Express to create a scatter plot
+fig = px.scatter(tips, x='total_bill', y='tip', color='sex', size='size',hover_data=['day'], title='Total Bill vs Tip')
+fig.show()
