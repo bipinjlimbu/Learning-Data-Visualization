2f1e05cac4b25b6d24fef0073001a7424fa6f2a7
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-04 07:25:16 +0545

    Bar Plot in Plotly

--- Plotly/1.BasicPlots.py
+++ Plotly/1.BasicPlots.py
@@ -9,6 +9,14 @@ tips = sns.load_dataset('tips')
 
 pio.renderers.default = 'browser'
 
-# Using Plotly Express to create a scatter plot
+# Line Plot
+fig = px.line(tips, x='total_bill', y='tip', title='Total Bill vs Tip')
+fig.show()
+
+# Scatter Plot
 fig = px.scatter(tips, x='total_bill', y='tip', color='sex', size='size',hover_data=['day'], title='Total Bill vs Tip')
+fig.show()
+
+# Bar Plot
+fig = px.bar(tips, x='day', y='total_bill', color='sex', title='Total Bill by Day and Gender')
 fig.show()
