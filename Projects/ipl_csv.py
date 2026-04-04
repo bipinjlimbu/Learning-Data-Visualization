e143f9a3a9961512d0d270b091820adad868bda5
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-04 19:48:36 +0545

    Dataframe Info Checked

--- Projects/ipl_csv.py
+++ Projects/ipl_csv.py
@@ -4,3 +4,6 @@ df = pd.read_csv(r'C:\Users\Dell\Desktop\DataVisualization\Projects\ipl.csv')
 
 if __name__ == "__main__":
     print(df.head())
+    
+    # Information about the DataFrame
+    print(df.info())
